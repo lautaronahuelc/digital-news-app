fe40308e708923de2458c9407c0c723a26d13f45
lautaronahuelc <lautaronahuel.coria@gmail.com>
2025-01-21 00:02:14 -0300

    Updated_20250121.1

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,5 +1,78 @@
-import React from "react";
+'use client';
+
+import React, { useEffect } from "react";
+
+import HeroSlide from "@/components/HeroSlide";
+import { heroSlides } from "@/data/data";
+
+import './hero.css';
+
+// import AOS
+import AOS from 'aos';
+
+// import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 export default function Hero() {
-  return <div>Hero</div>;
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false,
+      mirror: false,
+    });
+  }, []);
+
+  return (
+    <section id="hero-slider" className="hero-slider">
+      <div className="container-md" data-aos='fade-in'>
+        <div className="row">
+          <div className="col-12">
+            <Swiper
+              slidesPerView={'auto'}
+              speed={500}
+              autoplay={{
+                delay: 3000,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                el: '.swiper-pagination',
+                type: 'bullets',
+                clickable: true,
+              }}
+              navigation={{
+                nextEl: '.custom-swiper-button-next',
+                prevEl: '.custom-swiper-button-prev',
+              }}
+              modules={[Autoplay, Pagination, Navigation]}
+              loop={true}
+              className="sliderFeaturedPosts"
+            >
+              {heroSlides.map(slide => (
+                <SwiperSlide key={slide.id}>
+                  <HeroSlide slide={slide} />
+                </SwiperSlide>
+              ))}
+
+              <div className="custom-swiper-button-next">
+                <span className="bi-chevron-right"></span>
+              </div>
+              <div className="custom-swiper-button-prev">
+                <span className="bi-chevron-left"></span>
+              </div>
+
+              <div className="swiper-pagination"></div>
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/sections/Posts.tsx
+++ src/sections/Posts.tsx
@@ -1,5 +1,142 @@
-import React from "react";
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState, useEffect } from "react";
+
+import Preloader from "@/components/Preloader";
+import PostItemOne from "@/components/PostItemOne";
+import TrendingPost from "@/components/TrendingPost";
+
+import "./posts.css";
 
 export default function Posts() {
-  return <div>Posts</div>;
+  const router = useRouter();
+  const [items, setItems] = useState<any | []>([]);
+  const [item, setItem] = useState({});
+
+  const getItemsData = () => {
+    fetch("/api/postitems")
+      .then((res) => res.json())
+      .then((data) => setItems(data))
+      .catch((e) => console.log(e.message));
+  };
+
+  const getSinglePostData = (id: string) => {
+    fetch(`api/postitems/${id}`)
+      .then((res) => {
+        if (res.status === 404) {
+          router.push("/not-found");
+        }
+        return res.json();
+      })
+      .then((data) => setItem(data))
+      .catch((e) => console.log(e.message));
+  };
+
+  useEffect(() => {
+    getItemsData();
+    getSinglePostData("678ef5b1b748c355673e4ebc");
+  }, []);
+
+  return (
+    <section id="posts" className="posts">
+      <div className="container" data-aos="fade-up">
+        <div className="row g-5">
+          <div className="col-lg-4">
+            <PostItemOne large={true} item={item} />
+          </div>
+          <div className="col-lg-8">
+            <div className="row g-5">
+              <div className="col-lg-4 border-start custom-border">
+                {items && items.length > 0 ? (
+                  items
+                    .filter(
+                      (item: { trending: boolean; top: boolean }) =>
+                        !item.trending && !item.top
+                    )
+                    .slice(0, 3)
+                    .map(
+                      (item: {
+                        _id: string;
+                        img: string;
+                        category: string;
+                        date: string;
+                        title: string;
+                        brief: string;
+                        avatar: string;
+                        author: string;
+                      }) => (
+                        <PostItemOne key={item._id} large={false} item={item} />
+                      )
+                    )
+                ) : (
+                  <Preloader />
+                )}
+              </div>
+              <div className="col-lg-4 border-start custom-border">
+                {items && items.length > 0 ? (
+                  items
+                    .filter(
+                      (item: { trending: boolean; top: boolean }) =>
+                        !item.trending && !item.top
+                    )
+                    .slice(3, 6)
+                    .map(
+                      (item: {
+                        _id: string;
+                        img: string;
+                        category: string;
+                        date: string;
+                        title: string;
+                        brief: string;
+                        avatar: string;
+                        author: string;
+                      }) => (
+                        <PostItemOne key={item._id} large={false} item={item} />
+                      )
+                    )
+                ) : (
+                  <Preloader />
+                )}
+              </div>
+              <div className="col-lg-4">
+                <div className="trending">
+                  <h3>Trending</h3>
+                  <ul className="trending-post">
+                    {items && items.length > 0 ? (
+                      items
+                        .filter((item: { trending: boolean }) => item.trending)
+                        .map(
+                          (
+                            item: {
+                              _id: string;
+                              img: string;
+                              category: string;
+                              date: string;
+                              title: string;
+                              brief: string;
+                              avatar: string;
+                              author: string;
+                            },
+                            index: number
+                          ) => (
+                            <TrendingPost
+                              key={item._id}
+                              index={index}
+                              item={item}
+                            />
+                          )
+                        )
+                    ) : (
+                      <Preloader />
+                    )}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
